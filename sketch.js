a4932b578af636277f5d9cb87c9b8f3b89a4d7c3
sarahayu <46186848+sarahayu@users.noreply.github.com>
2020-10-27 15:04:54 -0700

    optimized rendering
    optimized for shadow rendering by batch drawing most bubbles

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const FPS = 60,
     FRAME_LEN = 1 / FPS;
+const INTRO_BUBBLE_EVERY_N_FRAME = 4;
 const RESTITUTION = 0.1;
 
 const MAX_RADIUS = 100;
 const RADIUS_EXPAND = 10; // how much physical circle expands when hovered
 const MAX_STROKE = 10; // stroke thickiness for largest circle
 const HOVER_STROKE = 30; // actual stroke for when hovered
+const MAX_BUBBLES = 70;
 
 let gravity;
 let velocityFactor; // to prevent bubbles continuously moving after clumping together
@@ -13,11 +15,12 @@ let velocityDecreaseRate = 0.99995;
 
 let frame = 0;
 let bubbleCanvas;
+let bubbleQueue = [];
 let bubbles = [];
 let font;
 let bubbleGrabbed = false;
-let prioritizedBubble; // used to render grabbed bubble in front of all others
 let circleMask;
+let buffer;
 
 function BubbleAnimation(radius)
 {
@@ -33,7 +36,7 @@ BubbleAnimation.prototype.update = function (deltaTime)
     this.radius = min(
         max(this.radius + (this.expanding ? 1 : -1) * deltaTime * 50, this.constRadius),
         this.constRadius + 10);
-    this.apparentRadius = this.radius + (this.radius - this.constRadius) * 3;
+    this.apparentRadius = this.radius + (this.radius - this.constRadius) * 5;
     this.stroke = lerp(MAX_STROKE * (this.apparentRadius / (MAX_RADIUS + RADIUS_EXPAND)), HOVER_STROKE, this.percentOfFullSize());
 }
 
@@ -75,36 +78,56 @@ Bubble.prototype.update = function ()
     if (!this.dragged)
     {
         this.velocity.add(getGravVector(this.location));
-        this.velocity.mult(velocityFactor);
+        // if it's offscreen, disregard velocityFactor slowdown to give it an opportunity to enter screenspace
+        if (onScreen(this.location, 0))
+            this.velocity.mult(velocityFactor);
         this.location.add(p5.Vector.mult(this.velocity, FRAME_LEN));
     }
     else
         this.location = createVector(mouseX, mouseY);
 }
 
-Bubble.prototype.draw = function ()
+Bubble.prototype.draw = function (ctx, batch)
 {
-    // draw white border
-    push();
-    drawingContext.shadowOffsetX = 2;
-    drawingContext.shadowOffsetY = 2;
-    drawingContext.shadowBlur = 8;
-    drawingContext.shadowColor = 'rgba(0,0,0,0.2)';
-    noStroke();
-    fill('white');
-    ellipse(this.location.x, this.location.y, this.animation.apparentRadius * 2);
-    pop();
+    if (batch)
+    {
+        // draw white border
+        ctx.noStroke();
+        ctx.fill('white');
+        ctx.ellipse(this.location.x, this.location.y, this.animation.apparentRadius * 2);
 
-    // draw inner circle
-    push();
-    noStroke();
-    //fill('#FEEAE0');
-    image(this.image, this.location.x, this.location.y, this.animation.apparentRadius * 2 - this.animation.stroke * 2, this.animation.apparentRadius * 2 - this.animation.stroke * 2);
-    //ellipse(this.location.x, this.location.y, this.animation.apparentRadius * 2 - this.animation.stroke * 2);
-    pop();
+        // draw inner circle
+        ctx.noFill();
+        ctx.image(this.image, this.location.x, this.location.y, this.animation.apparentRadius * 2 - this.animation.stroke * 2, this.animation.apparentRadius * 2 - this.animation.stroke * 2);
+    }
+    else
+    {
 
-    if (this.animation.radius != this.constRadius)
-        drawCurvedText('Raiyaaan', this.animation.apparentRadius - this.animation.stroke + (this.animation.stroke * 0.2), this.animation.stroke, this.location.x, this.location.y, this.animation.expanding ? this.animation.percentOfFullSize() - 1 : 1 - this.animation.percentOfFullSize(), `rgba(0,0,0,${this.animation.percentOfFullSize()})`);
+        // draw white border
+        ctx.push();
+        ctx.noStroke();
+        ctx.fill('white');
+        ctx.drawingContext.shadowOffsetX = 2;
+        ctx.drawingContext.shadowOffsetY = 2;
+        ctx.drawingContext.shadowBlur = 8;
+        ctx.drawingContext.shadowColor = 'rgba(0,0,0,0.5)';
+        ctx.ellipse(this.location.x, this.location.y, this.animation.apparentRadius * 2);
+        ctx.pop();
+
+        // draw inner circle
+        ctx.push();
+        ctx.noStroke();
+        ctx.noFill();
+        ctx.fill('#FEEAE0');
+        ctx.ellipse(this.location.x, this.location.y, this.animation.apparentRadius * 2 - this.animation.stroke * 2);
+        ctx.image(this.image, this.location.x, this.location.y, this.animation.apparentRadius * 2 - this.animation.stroke * 2, this.animation.apparentRadius * 2 - this.animation.stroke * 2);
+        ctx.pop();
+    }
+}
+
+Bubble.prototype.drawLabel = function (ctx)
+{
+    drawCurvedText('Raiyaaan', this.animation.apparentRadius - this.animation.stroke + (this.animation.stroke * 0.2), this.animation.stroke, this.location.x, this.location.y, this.animation.expanding ? this.animation.percentOfFullSize() - 1 : 1 - this.animation.percentOfFullSize(), `rgba(0,0,0,${this.animation.percentOfFullSize()})`, ctx);
 }
 
 function resolveCollision(bubble1, bubble2)
@@ -163,9 +186,14 @@ function getOffscreenPoint()
     do {
         point = createVector(Math.random() * (windowWidth + 1000) - 500, Math.random() * (windowHeight + 1000) - 500);
     }
-    while (point.x >= -100 && point.x <= windowWidth + 100 &&
-        point.y >= -100 && point.y <= windowHeight + 100);
+    while (onScreen(point, 100));
     return point;
+}
+
+function onScreen(point, border)
+{
+    return point.x >= -border && point.x <= windowWidth + border &&
+        point.y >= -border && point.y <= windowHeight + border;
 }
 
 function getGravVector(curLoc)
@@ -188,31 +216,47 @@ function getGravVector(curLoc)
     return gravVector;
 }
 
-function drawCurvedText(str, radius, scale, x, y, offset, color)
+function drawCurvedText(str, radius, scale, x, y, offset, color, ctx)
 {
-    textFont('Courier New', scale);
+    ctx.textFont('Courier New', scale);
 
     let arcLength = 0;
-    let totalAngle = textWidth(str) / radius;
+    let totalAngle = ctx.textWidth(str) / radius;
 
     for (let i = 0; i < str.length; i++)
     {
         let currentChar = str.charAt(i);
-        let w = textWidth(currentChar);
+        let w = ctx.textWidth(currentChar);
 
         arcLength += w / 2;
         let theta = arcLength / radius - totalAngle / 2 + offset;
 
-        push();
-        fill(color);
-        translate(x, y);
-        rotate(theta);
-        translate(0, -radius);
-        text(currentChar, 0, 0);
-        pop();
+        ctx.push();
+        ctx.fill(color);
+        ctx.translate(x, y);
+        ctx.rotate(theta);
+        ctx.translate(0, -radius);
+        ctx.text(currentChar, 0, 0);
+        ctx.pop();
 
         arcLength += w / 2;
     }
+}
+
+function preload()
+{
+    circleMask = createGraphics(200, 200);
+    circleMask.circle(100, 100, 200);
+
+    for (let i = 0; i < MAX_BUBBLES; i++)
+        (function (j)
+        {
+            loadImage(`https://picsum.photos/${Math.floor(Math.random() * 300) + 100}`, img =>
+            {
+                let offscreen = getOffscreenPoint();
+                bubbleQueue.push(new Bubble(Math.pow((MAX_BUBBLES - j) / MAX_BUBBLES, 2) * 60 + 40, offscreen.x, offscreen.y, img));
+            })
+        })(i);
 }
 
 function setup()
@@ -224,11 +268,13 @@ function setup()
     bubbleCanvas.parent('bubble-area');
 
     frameRate(FPS);
-    textAlign(CENTER, BASELINE);
-    imageMode(CENTER);
-
-    circleMask = createGraphics(200, 200);
-    circleMask.circle(100, 100, 200);
+    buffer = createGraphics(windowWidth, windowHeight);
+    buffer.textAlign(CENTER, BASELINE);
+    buffer.imageMode(CENTER);
+    /* bubbleQueue.sort((first, second) =>
+ {
+     return first.radius - second.radius;
+ });*/
 }
 
 function draw()
@@ -237,48 +283,84 @@ function draw()
 
     velocityFactor = max(velocityFactor *= velocityDecreaseRate, 0.2);
 
-    if (frame < 2 * 60)
+    if (bubbleQueue.length != bubbles.length)
     {
-        if (frame % 2 == 0)
+        if (frame % INTRO_BUBBLE_EVERY_N_FRAME == 0)
         {
-            (function (i)
+            bubbles.push(bubbleQueue[bubbles.length]);
+            bubbles.sort((first, second) =>
             {
-                loadImage(`https://picsum.photos/${Math.floor(Math.random() * 300) + 100}`, img =>
-                {
-                    let offscreen = getOffscreenPoint();
-                    bubbles.push(new Bubble(Math.pow((60 - i / 2) / 60, 2) * 60 + 40, offscreen.x, offscreen.y, img));
-                })
-            })(frame);
+                return second.radius - first.radius;
+            });
         }
         frame++;
     }
     // have velocity decrease at a faster rate once bubbles have settled down
-    else if (frame == 2 * 60)
+    else if (frame == INTRO_BUBBLE_EVERY_N_FRAME * bubbleQueue.length - INTRO_BUBBLE_EVERY_N_FRAME + 1)
     {
+        console.log('Slowing down now');
         frame++;
         setTimeout(() =>
         {
-            velocityDecreaseRate = 0.9995;
-        }, 5000);
+            velocityDecreaseRate = 0.998;
+        }, 2000);
     }
 
     cursor(ARROW);
 
     for (const bubble of bubbles)
         bubble.update();
-
     for (const bubble1 of bubbles)
         for (const bubble2 of bubbles)
             if (bubble1 != bubble2)
                 resolveCollision(bubble1, bubble2);
 
-    for (const bubble of bubbles.slice(0).reverse()) // reverse order we draw so larger bubbles render on top of smaller ones
-        if (!bubble.animation.expanding)
-            bubble.draw();
-        else
-            prioritizedBubble = bubble;
+    buffer.clear();
 
-    if (prioritizedBubble) prioritizedBubble.draw();
+    // used to order rendering, which is:
+    // 1. non-expanding, background bubbles
+    // 2. bubbles with text
+    // 3. expanding, grabbed bubble
+    let expandingBubble;
+    let bubblesWithText = [];
+
+    for (const bubble of bubbles)
+    {
+        if (bubble.animation.expanding)
+            expandingBubble = bubble;
+        else if (bubble.animation.radius != bubble.animation.constRadius)
+            bubblesWithText.push(bubble);
+        else
+            bubble.draw(buffer, true);
+    }
+
+    push();
+    drawingContext.shadowOffsetX = 2;
+    drawingContext.shadowOffsetY = 2;
+    drawingContext.shadowBlur = 8;
+    drawingContext.shadowColor = 'rgba(0,0,0,0.5)';
+    image(buffer, 0, 0);
+    pop();
+    //buffer.clear();
+
+    push();
+
+    textAlign(CENTER, BASELINE);
+    imageMode(CENTER);
+    for (const bubble of bubblesWithText)
+    {
+        bubble.draw(this, false);
+        bubble.drawLabel(this);
+    }
+
+    if (expandingBubble)
+    {
+        expandingBubble.draw(this, false);
+        expandingBubble.drawLabel(this);
+    }
+    pop();
+
+    //image(buffer, 0, 0);
 }
 
 function windowResized()
